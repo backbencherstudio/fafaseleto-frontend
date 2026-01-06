0c13d6538966389437683b3596696f04d77eecfd
hrid1 <hridoycse1@gmail.com>
2026-01-06 18:02:39 +0600

    Price section start

--- components/main/PricingPlan.tsx
+++ components/main/PricingPlan.tsx
@@ -1,3 +1,4 @@
+import CircleCheckIcon from "../icons/CircleCheckIcon";
 import SectionHeading from "../reuseable/SectionHeading";
 
 export default function PricingPlan() {
@@ -10,6 +11,129 @@ export default function PricingPlan() {
           Whether you just want to evaluate your CV or aim for a role at BCG,
           Goldman Sachs, or LVMH, we have a plan for you.
         </p>
+      </div>
+
+      {/* cards */}
+
+      <div>
+        <div className="max-w-[1300px] mx-auto px-4 md:px-0 flex flex-col md:flex-row justify-center md:justify-between gap-5 mt-10 mb-16 items-center">
+          {/* Card 1 */}
+          <div className="w-[305px] max-h-[456px]  [background:#FAFAFA] shadow-[0_2px_30px_1px_rgba(0,0,0,0.07)] rounded-lg p-6">
+            <div>
+              <p className="text-[#0D1B2A]  text-xl font-normal leading-[normal] opacity-80 mb-[5px]">
+                Free
+              </p>
+              <h3 className="mb-5">
+                <span className="text-[color:var(--1,#0D1B2A)] text-center font-secondary text-[44px] font-medium leading-[normal] tracking-[-1px]">
+                  €0
+                </span>{" "}
+                <span className="text-[#0D1B2A] opacity-80">per month</span>{" "}
+              </h3>
+
+              <hr className="border-t opacity-20 [background:#0D1B2A] mb-5" />
+            </div>
+            <div className=" flex flex-col justify-between  gap-20">
+              <div>
+                {[
+                  "Sign up and evaluate your CV for free",
+                  "Get a score out of 100 with actionable tips",
+                  "Perfect for testing",
+                ].map((feature, index) => (
+                  <div className="flex items-start gap-3 mb-2.5" key={index}>
+                    <CircleCheckIcon className="shrink-0 mt-1 text-[#10B981]  " />
+                    <p className=" text-[#120928] text-base font-normal leading-[25px] opacity-80">
+                      {feature}
+                    </p>
+                  </div>
+                ))}
+              
+              </div>
+
+              <button className="w-full h-[53px] [background:var(--3,#10B981)] rounded-md text-[color:var(--2,#FAFAFA)] text-center  text-lg font-normal leading-[normal] tracking-[-0.5px] cursor-pointer">
+                Evaluaate my CV
+              </button>
+            </div>
+          </div>
+          {/* Card 2 */}
+          <div className="w-[305px] max-h-[456px]  [background:#FAFAFA] shadow-[0_2px_30px_1px_rgba(0,0,0,0.07)] rounded-lg p-6">
+            <div>
+              <p className="text-[#0D1B2A]  text-xl font-normal leading-[normal] opacity-80 mb-[5px]">
+                Starter
+              </p>
+              <h3 className="mb-5">
+                <span className="text-[color:var(--1,#0D1B2A)] text-center font-secondary text-[44px] font-medium leading-[normal] tracking-[-1px]">
+                  €4.90
+                </span>{" "}
+                <span className="text-[#0D1B2A] opacity-80">per month</span>{" "}
+              </h3>
+
+              <hr className="h-px opacity-20 [background:#0D1B2A] mb-5" />
+
+              <div className="flex flex-col justify-between gap-12.5">
+                <div className="">
+                  {[
+                    "Your first optimized CV",
+                    "Generate a perfect CV",
+                    "Downloadable in PDF format",
+                    "Choose from templates: Finance, Consulting, or Startup/VC",
+                  ].map((feature, index) => (
+                    <div className="flex items-start gap-3 mb-2.5" key={index}>
+                      <CircleCheckIcon className="shrink-0  text-[#10B981]  " />
+                      <p className=" text-[#120928] text-base font-normal leading-[25px] opacity-80">
+                        {feature}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+
+                <button className="w-full h-[53px] [background:var(--3,#10B981)] rounded-md text-[#FAFAFA] text-center  text-lg font-normal leading-[normal] tracking-[-0.5px]">
+                  Generate my CV
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* Card 3 */}
+          <div className="w-[305px]  [background:#0D1B2A] shadow-[0_2px_30px_1px_rgba(0,0,0,0.07)] rounded-lg p-6">
+            <div>
+              <p className="text-[#FAFAFA]  text-xl font-normal leading-[normal] opacity-80 mb-[5px]">
+                Standard
+              </p>
+              <h3 className="mb-5 text-[#FAFAFA]">
+                <span className=" text-center font-secondary text-[44px] font-medium leading-[normal] tracking-[-1px]">
+                  €24.90
+                </span>{" "}
+                <span className=" opacity-80">per month</span>{" "}
+              </h3>
+
+              <hr className=" h-px opacity-20 [border-color:#FAFAFA] mb-5" />
+
+              <div className="flex flex-col justify-between gap-22">
+                <div>
+                  {[
+                    "Generate unlimited CVs",
+                    "Access to all premium templates",
+                    "Personalized recommendations",
+                    "Cover letters included",
+                  ].map((feature, index) => (
+                    <div className="flex items-start gap-3 mb-2.5" key={index}>
+                      <CircleCheckIcon className="shrink-0  currentColor[#10B981] rounded-full text-white bg-[#10B981]  " />
+                      <p className=" text-[#FAFAFA] text-base font-normal leading-[25px] opacity-80">
+                        {feature}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+
+                <button className="w-full h-[53px] [background:var(--3,#10B981)] rounded-md text-[color:var(--2,#FAFAFA)] text-center  text-lg font-normal leading-[normal] tracking-[-0.5px] cursor-pointer">
+                  Evaluaate my CV
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* cards 4 */}
+          <div className="w-[305px]"></div>
+        </div>
       </div>
 
       {/* bottom curve */}

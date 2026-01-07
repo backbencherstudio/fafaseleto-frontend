cdcd58a1b934669445332e5db60f5aaa7eef074f
hrid1 <hridoycse1@gmail.com>
2026-01-07 12:43:07 +0600

    Testimonials section done

--- components/main/WhyPostulae.tsx
+++ components/main/WhyPostulae.tsx
@@ -1,28 +1,37 @@
 import Image from "next/image";
-import React from "react";
 import SectionHeading from "../reuseable/SectionHeading";
+import TestimonialSlider from "./TestimonialsSlider";
 
 export default function WhyPostulae() {
   return (
     <div>
       <div className="bg-[#F4F4F5]  pt-18">
-        <SectionHeading className="mb-12.5">Why Postulae?</SectionHeading>
-        <section className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-5 px-4">
-          <FeatureCard
-            imageSrc="/images/postulae1.svg"
-            title="Thought out by the elite"
-            description="Our templates were designed by graduates of HEC and Polytechnique"
-          />
-          <FeatureCard
-            imageSrc="/images/postulae2.svg"
-            title="A powerful algorithm"
-            description="Instant recommendations based on your profile, sector, and goal."
-          />
-          <FeatureCard
-            imageSrc="/images/postulae3.svg"
-            title="Premium templates"
-            description="Layouts designed to convince even the most demanding recruiters."
-          />
+        <div>
+          <SectionHeading className="mb-12.5">Why Postulae?</SectionHeading>
+          <section className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-5 px-4">
+            <FeatureCard
+              imageSrc="/images/postulae1.svg"
+              title="Thought out by the elite"
+              description="Our templates were designed by graduates of HEC and Polytechnique"
+            />
+            <FeatureCard
+              imageSrc="/images/postulae2.svg"
+              title="A powerful algorithm"
+              description="Instant recommendations based on your profile, sector, and goal."
+            />
+            <FeatureCard
+              imageSrc="/images/postulae3.svg"
+              title="Premium templates"
+              description="Layouts designed to convince even the most demanding recruiters."
+            />
+          </section>
+        </div>
+
+        {/* testmonials */}
+
+        <section className="pt-20">
+          <SectionHeading>Testimonials</SectionHeading>
+          <TestimonialSlider />
         </section>
       </div>
       {/* bottom curve */}
